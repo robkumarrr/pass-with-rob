d1e94f00cf4f7b49b0fb1546bc3b9f994e6a0ed8
Robert <rob_kumar@outlook.com>
2023-09-28 17:46:01 -0400

    Changed theme, fixed splash btns and updated pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,22 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'PASS With Rob',
-			social: {
-				github: 'https://github.com/robkumarrr',
-			},
+			// social: {
+			// 	github: 'https://github.com/robkumarrr'
+			// },
 			sidebar: [
 				{
+					label: 'Information',
+					autogenerate: { directory: 'information'},
+				},
+				{
 					label: 'PASS Session Work',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Sept 25, 2023', link: '/session_code/09_25_2023/' },
-						{ label: 'Sept 29, 2023', link: ''}
-					],
-				}
+					autogenerate: { directory: 'session_code'},
+				},
 			],
 			logo: {
-				light: './public/favicon.svg',
+				light: './public/favicon-light.svg',
 				dark: './public/favicon_dark.svg'
-				// dark: ''
 			},
 			customCss: [
 				'./src/styles/custom.css'
